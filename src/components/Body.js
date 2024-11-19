f3ac9aaab0282f09875a7c64c0dbc6e7916b593a
Alina1520 <alinaa1520@gmail.com>
2024-11-19 17:11:35 +0200

    Add Alina Naryzhna Card

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={"/Surname.png"} alt="Alina Naryzhna" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Alina Naryzhna</h2>
+          <p style={styles.position}>Full-Stack Web Developer</p>
         </div>
       </div>
     </main>
