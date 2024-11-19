a2f5ac6fb2ffa3180bec5248501d574c0c6f66e9
Danylo14230 <[danylo14230@gmail.com]>
2024-11-19 18:15:24 +0200

    Add Danylo Smutok Card

--- src/components/Body.js
+++ src/components/Body.js
@@ -18,6 +18,13 @@ const Body = () => {
         </div>
       </div>
       <div style={styles.card}>
+        <img src={"/avatar-default.png"} alt="Danylo Smutok" style={styles.photo} />
+        <div style={styles.textContainer}>
+          <h2 style={styles.name}>Danylo Smutok</h2>
+          <p style={styles.position}>Product Owner</p>
+        </div>
+      </div>
+      <div style={styles.card}>
         <img src={"/Samoliuk.JPG"} alt="Sviatoslav Samoliuk" style={styles.photo} />
         <div style={styles.textContainer}>
           <h2 style={styles.name}>Sviatoslav Samoliuk</h2>
